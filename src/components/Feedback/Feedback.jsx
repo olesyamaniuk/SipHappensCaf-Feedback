748a7b3a5c2e4d49c645510dbb2b40cc7bf06df1
olesyamaniuk <maniukolesya222@gmail.com>
2024-10-03 14:54:49 +0300

    add icons

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,19 +1,34 @@
 import css from "./Feedback.module.css"
+import {BsEmojiGrinFill, BsEmojiNeutralFill, BsEmojiTearFill, BsEmojiSunglassesFill, BsEmojiHeartEyesFill } from "react-icons/bs";
 
 export default function Feedback({
-    value: { good, neutral, bad },
-    positiveFeedback,
-    totalFeedback
+  value: { good, neutral, bad },
+  totalFeedback
 }) {
-    return (
-        <div className={css.wrap}>
-            <div className={css.feedback}>
-                <p className={css.feedbackItem}>Good: {good}</p>
-                <p className={css.feedbackItem}>Neutral: {neutral}</p>
-                <p className={css.feedbackItem}>Bad: {bad}</p>
-                <p className={css.feedbackItem}>Total feedback: {totalFeedback}</p>
-                <p className={css.feedbackItem}>Positive: {positiveFeedback}%</p>
-            </div>
-        </div>
-    );
+  const positiveFeedback = Math.round((good / totalFeedback) * 100);
+
+ const getEmoji = (positiveFeedback) => {
+    if (positiveFeedback >= 80) {
+      return <BsEmojiHeartEyesFill className={css.icon} />;
+    } else if (positiveFeedback >= 50) {
+      return <BsEmojiGrinFill className={css.icon} />;
+    } else if (positiveFeedback >= 20) {
+      return <BsEmojiNeutralFill className={css.icon} />;
+    } else {
+      return < BsEmojiTearFill className={css.icon} />;
+    }
+  };
+
+  return (
+    <div className={css.wrap}>
+      <div className={css.feedback}>
+        <p className={css.feedbackItem}><BsEmojiGrinFill className={css.icon} />Good: {good}</p>
+        <p className={css.feedbackItem}><BsEmojiNeutralFill className={css.icon} />Neutral: {neutral}</p>
+        <p className={css.feedbackItem}><BsEmojiTearFill className={css.icon} />Bad: {bad}</p>
+        <p className={css.feedbackItem}><BsEmojiSunglassesFill className={css.icon} />Total feedback: {totalFeedback}</p>
+        <p className={css.feedbackItem}>Positive: {positiveFeedback}% {getEmoji(positiveFeedback)}</p>
+      </div>
+    </div>
+  );
 }
+
--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,13 +1,19 @@
 import css from "./Options.module.css"
 
+import { BsEmojiGrinFill, BsEmojiNeutralFill, BsEmojiTearFill, BsEmojiSmileUpsideDownFill } from "react-icons/bs";
+
 export default function Options({ onUpdate, resetFeedback, totalFeedback }) {
     return (
         <div className={css.feedback}>
-            <button className={css.feedbackItem} onClick={() => onUpdate("good")}>Good</button>
-            <button className={css.feedbackItem} onClick={() => onUpdate("neutral")}>Neutral</button>
-            <button  className={css.feedbackItem} onClick={() => onUpdate("bad")}>Bad</button>
+            <button className={css.feedbackItem} onClick={() => onUpdate("good")}>
+                <BsEmojiGrinFill className={css.icon} />Good</button>
+            <button className={css.feedbackItem} onClick={() => onUpdate("neutral")}>
+                <BsEmojiNeutralFill className={css.icon} />Neutral</button>
+            <button className={css.feedbackItem} onClick={() => onUpdate("bad")}>
+                <BsEmojiTearFill className={css.icon} />Bad</button>
             {totalFeedback > 0 && (
-                <button className={css.feedbackReset } onClick={resetFeedback}>Reset</button>
+                <button className={css.feedbackReset} onClick={resetFeedback}>
+                    <BsEmojiSmileUpsideDownFill className={css.icon} />Reset</button>
             )}
         </div>
     );
